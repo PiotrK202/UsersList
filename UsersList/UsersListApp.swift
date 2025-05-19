2b75dfc10ccaf4c6834cb1abff065ea6d3b1c74d
PiotrK <piotr.koscielny69@gmail.com>
2025-05-19 10:27:10 +0200

    Initial Commit

---
+++ UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by piotr koscielny on 19/5/25.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
