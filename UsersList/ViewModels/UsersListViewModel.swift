37d0c95dd4f1c84238e66adcbe4eb2e24c706da5
PiotrK <piotr.koscielny69@gmail.com>
2025-05-22 13:43:43 +0200

    users list viewModel update

--- UsersList/ViewModels/UsersListViewModel.swift
+++ UsersList/ViewModels/UsersListViewModel.swift
@@ -10,28 +10,27 @@ import Foundation
 @Observable
 final class UsersListViewModel {
     private let repository: RepositoryProtocol
-    private(set) var users = [User]()
+    private var currentPage = 1
+    private var totalPages = 1
+    var users = [User]()
     
     init(repository: RepositoryProtocol) {
         self.repository = repository
     }
     
     func fetchUsers() async throws {
-        do {
-            var page = 1
-            var allUsers: [User] = []
-            var totalPages: Int = 1
-            
-            repeat {
-                let response = try await repository.fetchUsers(page: page)
-                allUsers.append(contentsOf: response.data)
+        guard currentPage <= totalPages else { return }
+        
+        repeat {
+            do {
+                let response = try await repository.fetchUsers(page: currentPage)
+                users.append(contentsOf: response.data)
                 totalPages = response.totalPages
-                page += 1
-            } while page <= totalPages
-                        self.users = allUsers
-        } catch {
-            throw error
-        }
+                currentPage += 1
+            } catch {
+                throw error
+            }
+        } while currentPage <= totalPages
     }
     
     func deleteUser(at indexSet: IndexSet) async throws {
