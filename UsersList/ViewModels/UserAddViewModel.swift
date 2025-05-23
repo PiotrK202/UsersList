df5e5e6022fa7c7de066f2edad94b46879956408
PiotrK <piotr.koscielny69@gmail.com>
2025-05-23 13:22:54 +0200

    added view model fo add view

--- UsersList/ViewModels/UserAddViewModel.swift
+++ UsersList/ViewModels/UserAddViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+// MARK: ViewModel for adding user
+
+@Observable
+final class UserAddViewModel {
+    private let repository: RepositoryProtocol
+    var name = ""
+    var job = ""
+    
+    init(repository: RepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    func creatUser() async throws {
+        let request = CreateUserRequest(name: name, job: job)
+        do {
+           _ = try await repository.addUser(request)
+        } catch {
+            throw error
+        }
+    }
+}
