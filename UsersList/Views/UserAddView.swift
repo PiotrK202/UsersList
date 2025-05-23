12c6788bb32394e5d8e66a96674288fae8d0937e
PiotrK <piotr.koscielny69@gmail.com>
2025-05-23 13:23:02 +0200

    added user add view

--- UsersList/Views/UserAddView.swift
+++ UsersList/Views/UserAddView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct UserAddView: View {
+    @Environment(\.dismiss) private var dismiss
+    @Bindable private var viewModel: UserAddViewModel
+    @State private var isShowingAlert = false
+    
+    init(viewModel: UserAddViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("User Name") {
+                    TextField("name", text: $viewModel.name)
+                }
+                Section("User Job") {
+                    TextField("Job", text: $viewModel.job)
+                }
+                
+                Button("Add") {
+                    Task {
+                        do {
+                            try await viewModel.creatUser()
+                            dismiss()
+                        } catch {
+                            isShowingAlert = true
+                        }
+                    }
+                }
+            }
+            .alert(isPresented: $isShowingAlert) {
+                Alert(title: Text("Error"), message: Text("Adding user went wrong"), dismissButton: .cancel())
+            }
+        }
     }
 }
 
 #Preview {
-    UserAddView()
+    UserAddView(viewModel: UserAddViewModel(repository: Repository(dataService: DataService(session: URLSessionHelper.session))))
 }
