9f87dc4e82ca4080d5c48ba31491670ff3379283
PiotrK <piotr.koscielny69@gmail.com>
2025-05-26 10:17:45 +0200

    added user detail view model

---
+++ UsersListUnitTests/UserDetailViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  UserDetailViewModelTests.swift
+//  UsersListUnitTests
+//
+//  Created by piotr koscielny on 26/5/25.
+//
+
+import Testing
+
+struct UserDetailViewModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
