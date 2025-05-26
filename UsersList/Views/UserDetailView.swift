45de7a470f08bb528e43720190ac9cd96fc77e85
PiotrK <piotr.koscielny69@gmail.com>
2025-05-26 10:17:55 +0200

    added user detail view

--- UsersList/Views/UserDetailView.swift
+++ UsersList/Views/UserDetailView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct UserDetailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @Bindable private var viewModel: UserDetailViewModel
+    @State private var showAlert = false
+    @State private var localTextFieldName = ""
+    @State private var localTextFieldJob = ""
+    
+    init(viewModel: UserDetailViewModel) {
+        self.viewModel = viewModel
     }
-}
-
-#Preview {
-    UserDetailView()
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                AsyncImage(url: URL(string: viewModel.user.avatar)) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                    } else if phase.error != nil {
+                        Color.clear
+                            .frame(width: 100, height: 100)
+                    } else {
+                        ProgressView()
+                            .frame(width: 100, height: 100)
+                    }
+                }
+                Text(viewModel.user.email)
+                    .font(.subheadline)
+                
+                Text(viewModel.name)
+                    .font(.subheadline)
+                
+                Text(viewModel.job)
+                    .font(.subheadline)
+                
+                TextField("name", text: $localTextFieldName)
+                    .padding()
+                    .foregroundStyle(.gray)
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("job",text: $localTextFieldJob)
+                    .padding()
+                    .foregroundStyle(.gray)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button("update user") {
+                    Task {
+                        do {
+                            try await viewModel.updateUser(newName: localTextFieldName, newJob: localTextFieldJob)
+                        } catch {
+                            showAlert = true
+                        }
+                    }
+                }
+            }
+            .navigationTitle("detail")
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Error"), message: Text("Something went wrong"), dismissButton: .cancel())
+            }
+        }
+    }
 }
